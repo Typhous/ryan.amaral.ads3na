897b7132468fea0bc11ce239eeef619b0e1bb78b
Typhous <amaralryan4@gmail.com>
2025-03-23 21:51:14 +0000

    Adicionando projeto Rust

---
+++ target/debug/deps/aula4-ba7050d6fe1ee97f.d
@@ -0,0 +1,5 @@
+/workspaces/Typhous-ryan.amaral.ads1na/aula4/target/debug/deps/aula4-ba7050d6fe1ee97f: src/main.rs
+
+/workspaces/Typhous-ryan.amaral.ads1na/aula4/target/debug/deps/aula4-ba7050d6fe1ee97f.d: src/main.rs
+
+src/main.rs:
