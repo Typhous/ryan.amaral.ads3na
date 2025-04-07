b9e45c673c55b076339caf184ed2cea6ee42200c
Typhous <amaralryan4@gmail.com>
2025-04-07 00:24:54 +0000

    Atividade ponteiro

---
+++ target/debug/deps/aula5-0263761b6ea47afc.d
@@ -0,0 +1,5 @@
+/workspaces/Typhous-ryan.amaral.ads1na/aula5/target/debug/deps/aula5-0263761b6ea47afc: src/lib.rs
+
+/workspaces/Typhous-ryan.amaral.ads1na/aula5/target/debug/deps/aula5-0263761b6ea47afc.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/aula5-6b22d5ad3f8bdcd5.d
@@ -0,0 +1,7 @@
+/workspaces/Typhous-ryan.amaral.ads1na/aula5/target/debug/deps/libaula5-6b22d5ad3f8bdcd5.rmeta: src/lib.rs
+
+/workspaces/Typhous-ryan.amaral.ads1na/aula5/target/debug/deps/libaula5-6b22d5ad3f8bdcd5.rlib: src/lib.rs
+
+/workspaces/Typhous-ryan.amaral.ads1na/aula5/target/debug/deps/aula5-6b22d5ad3f8bdcd5.d: src/lib.rs
+
+src/lib.rs:
